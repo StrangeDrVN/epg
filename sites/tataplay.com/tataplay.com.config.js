cff9782e3382f7582f92081f14fbc96f2ff17c10
StrangeDrVN <172238701+StrangeDrVN@users.noreply.github.com>
2026-08-05 14:43:31 +0530

    Update maxConnections in tataplay.com config
    Reduced maxConnections from 4 to 2.

--- sites/tataplay.com/tataplay.com.config.js
+++ sites/tataplay.com/tataplay.com.config.js
@@ -4,7 +4,7 @@ module.exports = {
   site: 'tataplay.com',
   days: 1,
   delay: 8000,
-  maxConnections: 4,
+  maxConnections: 2,
 
   url({ date }) {
     return `https://tm.tapi.videoready.tv/content-detail/pub/api/v2/channels/schedule?date=${date.format(
